54bfceafb2f6b7166d23b9d62ba0fcc0b86f685e
razyees <rajeshpoudel9849@gmail.com>
2019-03-12 11:54:12 +0545

    Tweak - Autofocus and hide search on esc

--- js/search-toggle.js
+++ js/search-toggle.js
@@ -1,6 +1,25 @@
 jQuery(document).ready(function () {
+    var hideSearchForm = function() {
+        jQuery( '#masthead .masonic-search-toggle' ).hide( 'fast' );
+    };
     jQuery('.sb-search').click(function () {
         jQuery('.masonic-search-toggle').slideToggle('slow');
+
+        // focus after some time to fix conflict with toggle classs
+        setTimeout( function (  ) {
+            jQuery( '.masonic-search-toggle .masonic-search input' ).focus();
+
+        }, 200 );
+
+        // Hide search box on esc.
+        jQuery( document ).on( 'keyup', function (e) {
+            if ( 27 === e.keyCode ){
+
+                if (  jQuery( '#masthead .masonic-search-toggle' ) .has( '.masonic-search')  ) {
+                    hideSearchForm();
+                }
+            }
+        } );
     });
 });
 jQuery(document).ready(function () {
