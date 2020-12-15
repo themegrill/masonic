bba9f647a34689ecf0a380f5942d0ebaf17b51de
sakarrr <ctha.sakar@gmail.com>
2020-12-15 09:44:20 +0545

    Update - Footer Credentials

--- functions.php
+++ functions.php
@@ -232,11 +232,11 @@ add_filter('excerpt_more', 'masonic_read_more');
 if (!function_exists('masonic_footer_copyright')) :
 
    function masonic_footer_copyright() {
-      $wp_link = '<a href="' . 'https://wordpress.org' . '" target="_blank" title="' . esc_attr__('WordPress', 'masonic') . '"><span>' . __('WordPress', 'masonic') . '</span></a>';
+      $wp_link = '<a href="' . 'https://wordpress.org' . '" target="_blank" title="' . esc_attr__('WordPress', 'masonic') . '" rel="nofollow"><span>' . __('WordPress', 'masonic') . '</span></a>';
 
-      $tg_link = '<a href="' . 'https://themegrill.com/themes/masonic' . '" target="_blank" title="' . esc_attr__('ThemeGrill', 'masonic') . '" rel="author"><span>' . __('ThemeGrill', 'masonic') . '</span></a>';
+      $tg_link = '<a href="' . 'https://themegrill.com/themes/masonic' . '" target="_blank" title="' . esc_attr__('Masonic', 'masonic') . '" rel="nofollow"><span>' . __('Masonic', 'masonic') . '</span></a>';
 
-      $default_footer_value = sprintf(__('Powered by %s', 'masonic'), $wp_link) . ' <br> ' . sprintf(__('Theme: %2$s by %1$s', 'masonic'), $tg_link, 'Masonic');
+      $default_footer_value = sprintf(__('Powered by %s', 'masonic'), $wp_link) . ' <br> ' . sprintf(__('Theme: %2$s by %1$s', 'masonic'), 'ThemeGrill', $tg_link);
 
       $masonic_footer_copyright = $default_footer_value;
       echo $masonic_footer_copyright;
